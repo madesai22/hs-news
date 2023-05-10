96ca2648c5003b3eb07ec8041d9c6857cf628c4c
Meera Desai <meera.ann.desai@gmail.com>
2023-05-09 20:05:33 -0400

    add topic-modeling.py

--- explore-data.py
+++ explore-data.py
@@ -4,7 +4,7 @@ from datetime import datetime
 import matplotlib.pyplot as plt
 import pandas as pd 
 
-i = 0
+
 year_counts = {} # key: year (int) --> value: ['n gv headlines','n other','total'] (list)
 n_gv_headlines_idx = 0
 n_other_idx = 1
@@ -15,49 +15,49 @@ total_idx = 2
 with open('/data/madesai/articles_clean.jsonlist') as f, open('./gv-headlines.csv','w') as f2:
     
     for line in f:
-        
+            
 
-        data = json.loads(line)
-        headline = data['headline']
-        content = data['content']
-        if data['date']:
-        # try:
-        #     year = datetime.strptime(data['date'], "%B %d, %Y").year
-        # except:
-            year_pattern = r"(19[1-9][0-9]|20[0-9][0-9])"
+            data = json.loads(line)
+            headline = data['headline']
+            content = data['content']
+            if data['date']:
+            # try:
+            #     year = datetime.strptime(data['date'], "%B %d, %Y").year
+            # except:
+                year_pattern = r"(19[1-9][0-9]|20[0-9][0-9])"
 
-            year_string  = re.findall(year_pattern, data['date'])
-            if year_string:
-                year  = int(year_string[0])
+                year_string  = re.findall(year_pattern, data['date'])
+                if year_string:
+                    year  = int(year_string[0])
+                else:
+                    year = 3000
             else:
                 year = 3000
-        else:
-            year = 3000
-        pattern = re.compile(r"\b(gun)\b", re.IGNORECASE)
-        pattern2 = r"\<shoot(?!.*\b(?:ball|lacrosse|hoop)\b)"
-        pattern3 = r"\<shot(?!.*\b(?:ball|lacrosse|hoop)\b)"    
-        headline_gv = False
-        content_gv = False
+            pattern = re.compile(r"\b(gun)\b", re.IGNORECASE)
+            pattern2 = r"\<shoot(?!.*\b(?:ball|lacrosse|hoop)\b)"
+            pattern3 = r"\<shot(?!.*\b(?:ball|lacrosse|hoop)\b)"    
+            headline_gv = False
+            content_gv = False
 
-    
-        if re.findall(pattern, headline) or re.findall(pattern2, headline) or re.findall(pattern3, headline):
-            headline_gv = True
-            # i += 1 
-            f2.write(headline.replace(",", "")+','+str(year)+'\n')
-            #df.loc[df['year'] == year, 'n gv headlines'] += 1
-            if year in year_counts:
-                year_counts[year][n_gv_headlines_idx] += 1
-                year_counts[year][total_idx] += 1
+       
+            if re.findall(pattern, headline) or re.findall(pattern2, headline) or re.findall(pattern3, headline):
+                headline_gv = True
+
+                f2.write(headline.replace(",", "")+','+str(year)+'\n')
+                #df.loc[df['year'] == year, 'n gv headlines'] += 1
+                if year in year_counts:
+                    year_counts[year][n_gv_headlines_idx] += 1
+                    year_counts[year][total_idx] += 1
+                else:
+                    year_counts[year]= [1,0,1] 
             else:
-                year_counts[year]= [1,0,1] 
-        else:
 
-            if year in year_counts:
-                year_counts[year][n_other_idx] += 1
-                year_counts[year][total_idx] += 1
+                if year in year_counts:
+                    year_counts[year][n_other_idx] += 1
+                    year_counts[year][total_idx] += 1
 
-            else:
-                year_counts[year] = [0,1,1]
+                else:
+                    year_counts[year] = [0,1,1]
 f2.close()
 df = pd.DataFrame.from_dict(year_counts, orient='index', columns=[ 'n gv headlines','n other','total'])
 df.reset_index(inplace=True)
@@ -77,9 +77,9 @@ df.to_csv('gv-articles-by-year.csv')
 
 
 
-print(year_counts)
-print("missing year for "+str(no_date_gv)+" articles with gun violence.")
-print("missing year for "+str(no_date_other)+" other articles.")
+#print(year_counts)
+#print("missing year for "+str(no_date_gv)+" articles with gun violence.")
+#print("missing year for "+str(no_date_other)+" other articles.")
     
 # percentage plot
 #years = list(year_counts.keys())
