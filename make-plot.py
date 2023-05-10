807a57dbd7848333198e186a59755a31a74d82c5
Meera Desai <meera.ann.desai@gmail.com>
2023-05-10 16:14:58 +0000

    make plot

---
+++ make-plot.py
@@ -0,0 +1,44 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+
+df = pd.read_csv('gv-articles-by-year.csv')
+
+# Extract the rows with year values between 1999 and 2019
+start_year = 1999
+end_year = 2019
+data = df[(df['year'] >= start_year) & (df['year'] <= end_year)].sort_values(by='year')
+
+year = data['year']
+n_gv = data['n gv headlines']
+total = data['total']
+
+fig, ax = plt.subplots()
+ax.plot(year, n_gv), label='Gun Violence Headlines')
+ax.plot(year, total, label='Total Headlines')
+ax.set_xlabel('Year')
+ax.set_ylabel('n headlines')
+ax.set_title('Gun Violence headlines')
+ax.legend()
+
+
+ax.set_ylim(ymin=0)
+ax.set_xlim(xmin=1999,xmax=2019)
+ax.set_xticks(range(1999, 2020,2))
+#ax.set_yticks(range(0, int(max(total))+1))
+
+ax.xaxis.set_major_formatter(plt.FuncFormatter(lambda x, loc: "{:}".format(int(x))))
+
+# Add major and minor tick lines
+ax.yaxis.grid(which='major', linestyle='-', linewidth='0.5', color='grey', alpha=0.8)
+ax.yaxis.grid(which='minor', linestyle=':', linewidth='0.5', color='grey', alpha=0.4)
+ax.xaxis.grid(which='major', linestyle='-', linewidth='0.5', color='grey', alpha=0.8)
+ax.xaxis.grid(which='minor', linestyle=':', linewidth='0.5', color='grey', alpha=0.4)
+
+# Adjust the layout to add white space
+fig.tight_layout(pad=2)
+
+# Save the plot
+fig.savefig('gun-violence-headlines-by-year.png', dpi=300)
+
+
