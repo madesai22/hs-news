bc6fb5e153d82c8d846cc286ec63be601f753f0c
Meera Desai <meera.ann.desai@gmail.com>
2023-05-17 19:28:41 -0400

    explore data

--- explore_data.py
+++ explore_data.py
@@ -3,13 +3,16 @@ import file_handling as fh
 
 
 
-i = 0 
+high = 0 
+other = 0 
 with open('/data/madesai/articles_clean.jsonlist') as f:
     for line in f:
-        if i <25:
+        
+        if json.loads(line)['school_type'] == 'high':
+            high +=1
+        else:
             print(json.loads(line)['school_type'])
             
-        i +=1
 
     
 
