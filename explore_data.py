d9b5eb7a409485538d95fb873be0b8231b5a94a6
Meera Desai <meera.ann.desai@gmail.com>
2023-05-17 19:40:05 -0400

    explore data

--- explore_data.py
+++ explore_data.py
@@ -3,22 +3,11 @@ import file_handling as fh
 
 
 
-high = 0 
-college = 0 
-middle = 0 
-with open('/data/madesai/articles_clean.jsonlist') as f:
-    for line in f:
-        school_type = json.loads(line)['school_type']
-        if school_type == 'high':
-            high +=1
-        elif school_type == 'college':
-            college +=1
-        elif school_type == 'middle':
-            middle +=1
-        else:
-            print(school_type)
-
-print("{} middle, {} high, {} college".format(middle,high,college))
+#nschools = 0
+with open('/data/madesai/school_locations_full.jsonlist') as f:
+    print(len(f))
+        
+        
             
 
     
