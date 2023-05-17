68ab73cbeafec611db9cc2661f2492a447757316
Meera Desai <meera.ann.desai@gmail.com>
2023-05-17 19:24:46 -0400

    explore data

--- explore_data.py
+++ explore_data.py
@@ -7,7 +7,7 @@ i = 0
 with open('/data/madesai/articles_clean.jsonlist') as f:
     for line in f:
         if i <1:
-            print(json.loads(line).keys)
+            print(json.loads(line).keys())
             
         i +=2
 
