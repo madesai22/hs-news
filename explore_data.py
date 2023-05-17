bf5e3b7c7bbec5a02c342800e1889294dde6336b
Meera Desai <meera.ann.desai@gmail.com>
2023-05-17 18:09:16 -0400

    explore data

--- explore_data.py
+++ explore_data.py
@@ -5,6 +5,10 @@ import file_handling as fh
 
 json_file = '/data/madesai/school_full_info_with_votes.jsonlist'
 data = fh.read_jsonlist(json_file)
-print(data)
+for item in data:
+    try:
+        print(item.keys)
+    except:
+        print("no key")
 
 
