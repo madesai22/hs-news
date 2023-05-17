92a62324fef6e5df5828461bbb929e7173cc2f28
Meera Desai <meera.ann.desai@gmail.com>
2023-05-17 19:41:41 -0400

    explore data

--- explore_data.py
+++ explore_data.py
@@ -3,10 +3,13 @@ import file_handling as fh
 
 
 
-#nschools = 0
+nschools = 0
 with open('/data/madesai/school_locations_full.jsonlist') as f:
-    print(len(f))
-        
+    for line in f:
+        if json.loads(line)['school_name']:
+            nschools+=1
+
+print("{} schools".format(nschools))
         
             
 
