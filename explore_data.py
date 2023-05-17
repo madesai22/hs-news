48353f95a445e9844f61844dc7d9384c3c1289e0
Meera Desai <meera.ann.desai@gmail.com>
2023-05-17 18:19:14 -0400

    explore data

--- explore_data.py
+++ explore_data.py
@@ -3,9 +3,9 @@ import file_handling as fh
 
 
 
-json_file = '/data/madesai/articles_clean.jsonlist'
-data = fh.read_jsonlist(json_file)
-print(data[0])
+
+with open('/data/madesai/articles_clean.jsonlist') as f:
+    print f[0]
 
     
 
