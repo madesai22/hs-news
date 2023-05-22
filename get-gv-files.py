0582dab00443b0f3e229b0f58c0b6748b8e4d057
Meera Desai <meera.ann.desai@gmail.com>
2023-05-22 13:32:59 -0400

    make histogram

--- get-gv-files.py
+++ get-gv-files.py
@@ -40,24 +40,26 @@ def main():
     all_headlines = []
     n_gv = 0 
     total = 0
-    with open('/data/madesai/articles_clean.jsonlist') as f, open(path+'/gv-headlines.csv','w') as f2, open(path+'/shooting-headlines.csv','w') as f3:
+    with open('/data/madesai/articles_clean.jsonlist') as f, open(path+'/headline_counts.csv','w') as f2: #open(path+'/gv-headlines.csv','w') as f2, open(path+'/shooting-headlines.csv','w') as f3:
         for line in f:
             sys.stdout.write("Seen %.2f percent of articles\r" %(total/1951843*100))
             sys.stdout.flush()
             total +=1
             
             data = json.loads(line)
-            headline = data['headline']
-            content = data['content']
-            pp_content = pp.pre_process(content,stopwords)
+            #headline = data['headline']
+            #content = data['content']
+            school_type = data['school_type']
+            #pp_content = pp.pre_process(content,stopwords)
 
             if data['date']:
                 year = pp.get_year(data['date'])
             else:
                 year =  3000
+            f2.write(str(year)+","+school_type+'\n')
             
-            all_headlines.append(pp.pre_process(headline,stopwords))
-            all_content.append(pp_content)
+            #all_headlines.append(pp.pre_process(headline,stopwords))
+            #all_content.append(pp_content)
 
             # if pp.match_gun_violence(headline):
             #     gv_by_headline_json_file.append(line)
@@ -71,21 +73,21 @@ def main():
     sys.stdout.write("Writing files...")
     #fh.pickle_data(gv_by_content_content, path+'/gv_content_by_content.pkl')
     #fh.pickle_data(gv_by_headline_content, path+'/gv_content_by_headline.pkl')
-    fh.pickle_data(all_headlines,path +'/all_headlines.pkl')
-    fh.pickle_data(all_content, path+'/all_content.pkl')
+    #fh.pickle_data(all_headlines,path +'/all_headlines.pkl')
+    #fh.pickle_data(all_content, path+'/all_content.pkl')
     #fh.write_to_jsonlist(gv_by_headline_json_file,path+'/gun-violence-articles-by-headline_clean.jsonlist')
 
 
-    documentation = """file,description
-    gv-headlines.csv,headlines that match gun violence terms - not preprocessed
-    gv_content.pkl,list of full content of articles that match gun violence terms, where each article is preprocessed (list of list of strings)
-    all_headlines.pkl,list of headlines of all articles, where each headline is preprocessed (list of list of strings)
-    all_content.pkl,list of full content of articles, where each article is preprocessed (list of list of strings)
-    gun-violence-articles_clean.jsonlist,jsonlist file with all articles that match gun violence terms, no preprocessing
-    stopwords, """+ stopword_file+""" 
-    other preprocessing, remove punctuation remove extra white spaces words lowercased tokenized"""
+    # documentation = """file,description
+    # gv-headlines.csv,headlines that match gun violence terms - not preprocessed
+    # gv_content.pkl,list of full content of articles that match gun violence terms, where each article is preprocessed (list of list of strings)
+    # all_headlines.pkl,list of headlines of all articles, where each headline is preprocessed (list of list of strings)
+    # all_content.pkl,list of full content of articles, where each article is preprocessed (list of list of strings)
+    # gun-violence-articles_clean.jsonlist,jsonlist file with all articles that match gun violence terms, no preprocessing
+    # stopwords, """+ stopword_file+""" 
+    # other preprocessing, remove punctuation remove extra white spaces words lowercased tokenized"""
 
-    fh.write_documentation(documentation,path+"/README.txt")
+    # fh.write_documentation(documentation,path+"/README.txt")
     sys.stdout.write("Done!")
 
 
