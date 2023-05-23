93844939f3e205f08db39e165d74df8a5b0aeaa7
Meera Desai <meera.ann.desai@gmail.com>
2023-05-23 15:23:41 -0400

    add explore data

--- explore_data.py
+++ explore_data.py
@@ -12,35 +12,38 @@ def group_data_by_year(path_to_data, out_file_path, column = None, condition = N
     count = 0
     nfile_idx, nother_idx, total_idx = range(0,3)
     with open(path_to_data) as f:
-        for line in f: 
-            sys.stdout.write("Seen {} articles\r".format(count))
-            #sys.stdout.write("Seen %.2f percent of articles\r" %(count/nfiles*100))
-            sys.stdout.flush()
-            count +=1
-            
-            data = json.loads(line)
-            if condition and column:
-                take_file = data[column] == condition
-            else:
-                take_file == True
+        if count <500:
+            for line in f: 
+                sys.stdout.write("Seen {} articles\r".format(count))
+                #sys.stdout.write("Seen %.2f percent of articles\r" %(count/nfiles*100))
+                sys.stdout.flush()
+                count +=1
+                
+                data = json.loads(line)
+                if condition and column:
+                    take_file = data[column] == condition
+                else:
+                    take_file == True
 
-            if data['date']:
-                year = pp.get_year(data['date'])
-            else:
-                year =  3000
-            
-            if year in year_dict:
-                year_dict[year][total_idx] += 1
-                if take_file:
-                    year_dict[year][nfile_idx] += 1
+                if data['date']:
+                    year = pp.get_year(data['date'])
                 else:
-                    year_dict[year][nother_idx] += 1
-            else:
-                if take_file:
-                    year_dict[year] = [1,0,1]
+                    year =  3000
+                
+                if year in year_dict:
+                    year_dict[year][total_idx] += 1
+                    if take_file:
+                        year_dict[year][nfile_idx] += 1
+                    else:
+                        year_dict[year][nother_idx] += 1
                 else:
-                    year_dict[year] = [0,1,1]
+                    if take_file:
+                        year_dict[year] = [1,0,1]
+                    else:
+                        year_dict[year] = [0,1,1]
     for y in year_dict:
+        print(year_dict[y][nfile_idx])
+        print(year_dict[y][total_idx])
         pcent = (year_dict[y][nfile_idx])(year_dict[y][total_idx])*100
         year_dict[y].append(pcent)
     df = pd.DataFrame.from_dict(year_dict, orient='index', columns = ['nfiles','nother','total','percent'])
