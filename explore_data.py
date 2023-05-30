b9b49467bacf10a863646df1191a16f5bef5f587
Meera Desai <meera.ann.desai@gmail.com>
2023-05-30 09:10:12 -0400

    editing classify

--- explore_data.py
+++ explore_data.py
@@ -26,7 +26,7 @@ def domain_to_year(path_to_article_data, path_to_school_data,year_start=1999, ye
     for a in article_data:
         article_domain = a['domain']
         article_year = pp.get_year(a['date'])
-        if year in year_to_idx:
+        if article_year in year_to_idx:
             idx = year_to_idx[article_year]
             if article_domain in paper_dict:
                 paper_dict[article_domain][idx] += 1
