2cebcf1232342a9d3df5f0e194e26b59c987e1e8
Meera Desai <meera.ann.desai@gmail.com>
2023-06-06 13:27:35 -0400

    add

--- data_familiarity/time_distribution.py
+++ data_familiarity/time_distribution.py
@@ -1,11 +1,19 @@
 import sys
 sys.path.insert(1, '/home/madesai/hs-news/processing')
 import make_plot as mp
+import pandas as pd
+
 
 
 def main():
+    hs = pd.read_csv("/data/madesai/descriptive-statistics/n_high_articles_by_year.csv")
+    #college = pd.read_csv("/data/madesai/descriptive-statistics/n_college_articles_by_year.csv")
+    
 
-    path = "/data/madesai/descriptive-statistics/n_college_hs_by_year.csv"
+
+
+
+
     mp.plot_articles_by_year(path)
 
 if __name__ == '__main__':
