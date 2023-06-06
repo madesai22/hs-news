56618bf07ac4d31c855c9a4193a37a22915318ca
Meera Desai <meera.ann.desai@gmail.com>
2023-06-06 13:11:32 -0400

    add

--- data_familiarity/time_distribution.py
+++ data_familiarity/time_distribution.py
@@ -1,14 +1,15 @@
 import sys
 sys.path.insert(1, '/home/madesai/hs-news/processing')
 import make_plot as mp
-import explore_data as ed
 
 
 def main():
 
     path = "/data/madesai/descriptive-statistics/n_college_hs_by_year.csv"
-    # plot data
     mp.plot_articles_by_year(path)
+
+if __name__ == '__main__':
+    main()
 
 
 
