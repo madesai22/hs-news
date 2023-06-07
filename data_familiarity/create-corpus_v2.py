2e9456db045fc7b97a73dd5c3612a3808338926f
Meera Desai <meera.ann.desai@gmail.com>
2023-06-07 17:58:51 -0400

    add

--- data_familiarity/create-corpus_v2.py
+++ data_familiarity/create-corpus_v2.py
@@ -15,6 +15,14 @@ import sys
 import json
 import os
 
+def unpack_labels(labels_dict):
+    date = labels_dict['date']
+    school_type = labels_dict['school_type']
+    zip_code = labels_dict['zip_code']
+    is_foreign = labels_dict['is_foreign']
+    state = labels_dict['state']
+    return date, school_type, zip_code, is_foreign, state
+
 
 def get_meta_data(domain, meta_data_json):
     for school in meta_data_json:
@@ -32,6 +40,7 @@ def make_corpus(data,path_to_metadata, columns = [], conditions = []):
 
     labels = {'date':[], 'school_type':[], 'zip_code':[], 'is_foreign':[], 'dem_share':[], 'state':[]}
     total = 0 
+    duplicates = 0 
     for article in data:
         sys.stdout.write("Seen %d articles\r" %(total))
         sys.stdout.flush()
@@ -39,9 +48,7 @@ def make_corpus(data,path_to_metadata, columns = [], conditions = []):
         take_article = True
         for column, condition in zip(columns, conditions): # select for conditions
             if article[column] != condition:
-                print(article[column])
                 take_article = False
-        print(take_article)
         if take_article:
             text = article['content']
             if text not in seen: # remove duplicates 
@@ -57,6 +64,10 @@ def make_corpus(data,path_to_metadata, columns = [], conditions = []):
                 labels['dem_share'].append(meta_data['dem_share'])
                 labels['state'].append(meta_data['state'])
                 corpus.append(pp.pre_process(text,stopwords=None))
+        else:
+            duplicates += 1
+    print("found {} duplicates".format(duplicates))
+
     return corpus, labels
 
 def main():
@@ -65,11 +76,11 @@ def main():
         data = fh.read_jsonlist(random_sample)
     else:
         data = fh.read_jsonlist_random_sample('/data/madesai/student-news-full/all_articles_no_middle.jsonlist', .1, percent =True)
-        fh.write_to_jsonlist(data, random_sample)
-
+        fh.write_to_jsonlist(data, random_sample) 
+    #data = fh.read_jsonlist('/data/madesai/student-news-full/all_articles_no_middle.jsonlist')
     print('read random sample')
     path_to_md = '/data/madesai/student-news-full/school_full_info_with_votes.jsonlist'
-    corpus, columns = make_corpus(data,path_to_md, columns = ['school_type'], conditions = ['high'])
+    corpus, columns = make_corpus(data,path_to_md)# columns = ['school_type'], conditions = ['high'])
     print(corpus[:10])
 
     for c in columns:
